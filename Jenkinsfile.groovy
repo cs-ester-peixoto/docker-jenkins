5ca7df13f0de77c5fdfeefee7db8aa9fd0b06279
Ester Braga <ester.peixoto@concrete.com.br>
2017-01-26 15:29:11 -0200

    Add files via upload

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,5 +1,15 @@
 node{
-	stage('Message'){
-		echo 'The project has changed'
+	stage('Build'){
+		sh 'make'
+		archiveArtifacts artifacts: '**/target/*.jar', fingerprint: true
+	}
+	stage('Test'){
+		sh 'make check || true'
+		junit '**/target/*.xml'
+	}
+	stage('Deploy'){
+		if (currentBuild.result == 'SUCCESS') {
+			sh 'make publish'
+		}
 	}
 }
