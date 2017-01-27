6692900af8628673bd1abb728c56142314ef7cb8
Ester Braga <ester.peixoto@concrete.com.br>
2017-01-27 14:10:32 -0200

    Add files via upload

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,15 +1,19 @@
 node{
+	stage('Checkout'){
+		checkout scm
+	}
 	stage('Build'){
-		sh 'make'
-		archiveArtifacts artifacts: '**/target/*.jar', fingerprint: true
+		sh './dockerBuild.sh'
+		// archiveArtifacts artifacts: '**/target/*.jar', fingerprint: true
 	}
 	stage('Test'){
-		sh 'make check || true'
-		junit '**/target/*.xml'
+		//sh 'make check || true'
+		//junit '**/target/*.xml'
 	}
 	stage('Deploy'){
-		if (currentBuild.result == 'SUCCESS') {
-			sh 'make publish'
-		}
+		//if (currentBuild.result == 'SUCCESS') {
+		//	sh 'make publish'
+		//}
+		sh './dockerPushToRepo.sh'
 	}
 }
